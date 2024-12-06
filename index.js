b3078a06aac0d15362690ae57a4723d2e6568b7b
CamiloDxr <139256945+CamiloDxr@users.noreply.github.com>
2024-12-06 18:19:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("almacen.json");
 const middlewares = jsonServer.defaults();
-const cors = require("cors");  
+const cors = require("cors");
 const port = process.env.PORT || 10000;
 
 server.use(cors());
 
+server.use((req, res, next) => {
+  if (req.method === 'PUT' || req.method === 'PATCH') {
+    const body = req.body;
+
+    if (body.hasOwnProperty('role')) {
+      delete body.role; 
+    }
+
+    if (body.hasOwnProperty('isactive')) {
+      delete body.isactive; 
+    }
+  }
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
-server.listen(port,() => {
+server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
