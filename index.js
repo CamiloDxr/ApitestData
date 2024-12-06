64ba8666e1774da4a364a49a9d5508a8dc6dcd5b
CamiloDxr <139256945+CamiloDxr@users.noreply.github.com>
2024-12-06 17:59:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("almacen.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");  
 const port = process.env.PORT || 10000;
 
+server.use(cors());
 
 server.use(middlewares);
 server.use(router);
-server.listen(port);
+server.listen(port,() => {
+  console.log(`JSON Server is running on port ${port}`);
+});
